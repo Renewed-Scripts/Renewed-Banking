6f828e7fb33b22befb64e6aaea5e3b9bdd202bf5
AfterDarkRP <itzrishI@outlook.com>
2022-08-27 05:05:04 -0700

    feat: Bank Peds & qb-target Support

--- client/main.lua
+++ client/main.lua
@@ -7,7 +7,7 @@ local function openBankUI()
     SendNUIMessage({action = "setLoading", status = true})
     QBCore.Functions.TriggerCallback('renewed-banking:server:initalizeBanking', function(result)
         if not result then QBCore.Functions.Notify('Failed to load Banking Data!', 'error', 7500) end
-        SetTimeout(3500, function()
+        SetTimeout(1000, function()
             isVisible = true
             SendNUIMessage({
                 action = "setVisible",
@@ -32,6 +32,13 @@ RegisterNUICallback("closeInterface", function(_, cb)
 end)
 RegisterCommand("closeBankUI", function() closeBankUI() end)
 
+local targetOptions = {{
+    type = "client",
+    event = "Renewed-Banking:client:openBankUI",
+    icon = "fas fa-money-check",
+    label = "View Bank Account",
+}}
+
 local bankActions = {"deposit", "withdraw", "transfer"}
 CreateThread(function ()
     for k=1, #bankActions do
@@ -51,13 +58,49 @@ CreateThread(function ()
         `prop_atm_03`,
         `prop_fleeca_atm`
     },{
-        options = {{
-            type = "client",
-            event = "Renewed-Banking:client:openBankUI",
-            icon = "fas fa-money-check",
-            label = "View Bank Account",
-        },},
+        options = targetOptions,
         distance = 1.5
      })
 end)
 
+local pedSpawned = false
+local bankPeds = {}
+local function createPeds()
+    if pedSpawned then return end
+    for k=1, #config.peds do
+        local model = joaat(config.peds[k].model)
+
+        RequestModel(model)
+        while not HasModelLoaded(model) do Wait(0) end
+
+        local coords = config.peds[k].coords
+        bankPeds[k] = CreatePed(0, model, coords.x, coords.y, coords.z-1, coords.w, false, false)
+
+        TaskStartScenarioInPlace(bankPeds[k], 'PROP_HUMAN_STAND_IMPATIENT', 0, true)
+        FreezeEntityPosition(bankPeds[k], true)
+        SetEntityInvincible(bankPeds[k], true)
+        SetBlockingOfNonTemporaryEvents(bankPeds[k], true)
+
+        exports['qb-target']:AddTargetEntity(bankPeds[k], {
+            options = targetOptions,
+            distance = 2.0
+        })
+    end
+
+    pedSpawned = true
+end
+
+local function deletePeds()
+    if not pedSpawned then return end
+    for k=1, #bankPeds do
+        DeletePed(bankPeds[k])
+    end
+end
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+    createPeds()
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    deletePeds()
+end)
