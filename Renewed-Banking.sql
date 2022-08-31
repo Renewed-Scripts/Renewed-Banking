df43a18a8d4d34ae2c82246a28a1107e6717ba63
AfterDarkRP <itzrishI@outlook.com>
2022-08-31 10:19:26 -0700

    chore: Update SQL Schema

--- Renewed-Banking.sql
+++ Renewed-Banking.sql
@@ -1,10 +1,23 @@
-ALTER TABLE management_funds
-  ADD transactions longtext DEFAULT '[]',
-  ADD isFrozen int(11) DEFAULT 0;
+CREATE TABLE IF NOT EXISTS `bank_accounts_new` (
+  `id` varchar(50) NOT NULL,
+  `amount` int(11) DEFAULT 0,
+  `transactions` longtext DEFAULT '[]',
+  `auth` longtext DEFAULT '[]',
+  `isFrozen` int(11) DEFAULT 0,
+  `creator` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `bank_accounts_new` (`id`, `amount`, `transactions`, `auth`, `isFrozen`, `creator`) VALUES
+	('ambulance', 0, '[]', '[]', 0, NULL),
+	('cardealer', 0, '[]', '[]', 0, NULL),
+	('mechanic', 0, '[]', '[]', 0, NULL),
+	('police', 0, '[]', '[]', 0, NULL),
+	('realestate', 0, '[]', '[]', 0, NULL);
 
 CREATE TABLE IF NOT EXISTS `player_transactions` (
   `id` varchar(50) NOT NULL,
   `isFrozen` int(11) DEFAULT 0,
-  `transactions` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '[]',
+  `transactions` longtext DEFAULT '[]',
   PRIMARY KEY (`id`)
 );
