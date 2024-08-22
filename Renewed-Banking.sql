62456a2710f02390cd989659eaf12f39e7c6c78e
Solareon <769465+solareon@users.noreply.github.com>
2024-08-22 19:16:35 +0200

    refactor(sql): remove unneeded groups

--- Renewed-Banking.sql
+++ Renewed-Banking.sql
@@ -8,19 +8,6 @@ CREATE TABLE IF NOT EXISTS `bank_accounts_new` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `bank_accounts_new` (`id`, `amount`, `transactions`, `auth`, `isFrozen`, `creator`) VALUES
-	('ambulance', 0, '[]', '[]', 0, NULL),
-	('cardealer', 0, '[]', '[]', 0, NULL),
-	('mechanic', 0, '[]', '[]', 0, NULL),
-	('police', 0, '[]', '[]', 0, NULL),
-	('realestate', 0, '[]', '[]', 0, NULL),
-	('lostmc', 0, '[]', '[]', 0, NULL),
-	('ballas', 0, '[]', '[]', 0, NULL),
-	('vagos', 0, '[]', '[]', 0, NULL),
-	('cartel', 0, '[]', '[]', 0, NULL),
-	('families', 0, '[]', '[]', 0, NULL),
-	('triads', 0, '[]', '[]', 0, NULL);
-
 CREATE TABLE IF NOT EXISTS `player_transactions` (
   `id` varchar(50) NOT NULL,
   `isFrozen` int(11) DEFAULT 0,
