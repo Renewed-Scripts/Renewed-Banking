d8dbf3184c392859b9aec8191636873c58c3ea5a
Solareon <769465+solareon@users.noreply.github.com>
2024-08-29 07:39:23 +0000

    refactor(sql): use transaction for table creation

--- server/main.lua
+++ server/main.lua
@@ -660,22 +660,9 @@ function ExportHandler(resource, name, cb)
     end)
 end
 
-MySQL.query.await([=[
-    CREATE TABLE IF NOT EXISTS `bank_accounts_new` (
-    `id` varchar(50) NOT NULL,
-    `amount` int(11) DEFAULT 0,
-    `transactions` longtext DEFAULT '[]',
-    `auth` longtext DEFAULT '[]',
-    `isFrozen` int(11) DEFAULT 0,
-    `creator` varchar(50) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-    );
-]=])
-MySQL.query.await([=[
-    CREATE TABLE IF NOT EXISTS `player_transactions` (
-    `id` varchar(50) NOT NULL,
-    `isFrozen` int(11) DEFAULT 0,
-    `transactions` longtext DEFAULT '[]',
-    PRIMARY KEY (`id`)
-    );
-]=])
+local createTables = {
+    { query = "CREATE TABLE IF NOT EXISTS `bank_accounts_new` (`id` varchar(50) NOT NULL, `amount` int(11) DEFAULT 0, `transactions` longtext DEFAULT '[]', `auth` longtext DEFAULT '[]', `isFrozen` int(11) DEFAULT 0, `creator` varchar(50) DEFAULT NULL, PRIMARY KEY (`id`));", values = nil },
+    { query = "CREATE TABLE IF NOT EXISTS `player_transactions` (`id` varchar(50) NOT NULL, `isFrozen` int(11) DEFAULT 0, `transactions` longtext DEFAULT '[]', PRIMARY KEY (`id`));", values = nil }
+}
+
+assert(MySQL.transaction.await(createTables), "Failed to create tables")
